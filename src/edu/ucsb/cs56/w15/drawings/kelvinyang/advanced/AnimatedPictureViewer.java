0c035706be344f809011ed1d02744a13967b6db7
Kelvin Yang <kelvinyang@umail.ucsb.edu>
2015-02-16 18:08:52 -0800

    refactored threading and animation

--- src/edu/ucsb/cs56/w15/drawings/kelvinyang/advanced/AnimatedPictureViewer.java
+++ src/edu/ucsb/cs56/w15/drawings/kelvinyang/advanced/AnimatedPictureViewer.java
@@ -22,101 +22,71 @@ public class AnimatedPictureViewer extends JFrame
 
     /** constructs a JFrame with an animated picture inside
      */
-    public AnimatedPictureViewer() {
-        // If user passed a command line argument, 
-        // get which picture we want to display from the user
-        
-        // Set the size to whatever size you like (width, height)
-        // For projects you turn in, lets not get any bigger than 640,480        
+    public AnimatedPictureViewer()
+    {
+        // Set the size of canvas
         setSize(640,480);
         
-        // Set your own title
+        // Set the title of drawing
         setTitle("Kelvin Yang's Animated Drawing");
         
-        // Always do this so that the red X (or red circle) works
-        // to close the window. 
-        
+        //Allows window to close normally
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // Instantiate your drawing as a "component"
         component = new AnimatedPictureComponent(FAN_X, FAN_Y, FAN_HEIGHT, 20);
         
-        // Always add your component to the frame 
-        // and then make the window visible
-        
+        // Always add your component to the frame and then make the window visible
         add(component);
-
-        getContentPane().addMouseListener(
-                                                new MouseAdapter(){
-                                                    public void mouseEntered(MouseEvent e){
-                                                        //Get a new animation thread and start
-                                                        // the animation on it.
-                                                        animate = new Animate();
-                                                        animate.start();
-                                                    }//end mouseEntered
-                                                    
-                                                    public void mouseExited(MouseEvent e){
-                                                        //Terminate the animation.
-                                                        animate.interrupt();
-                                                        //Let the thread die a natural death.
-                                                        // Then make it eligible for garbage
-                                                        // collection.
-                                                        while (animate.isAlive()){}//loop;
-                                                        animate = null;
-                                                    }//end MouseExited
-                                                }//end new MouseAdapter
-                                                );//end addMouseListener()
-        //End definition of anonymous inner class
         setVisible(true);
+        
+        //Mouse listener for drawing to react to mouse events
+        getContentPane().addMouseListener( new MouseAdapter()
+        {
+            Animation a = new Animation(); //Create animation
+            Thread t = new Thread(a); //Create thread and add animation
+            boolean started = false;
+            
+            public void mouseEntered(MouseEvent e) //Mouse enter event
+            {
+                if(started)
+                    a.running = true; //Start animation
+                else
+                {
+                    t.start();
+                    started = true;
+                }
+            }
+            
+            public void mouseExited(MouseEvent e)
+            {
+                a.running = false; //Stop animation
+            }
+        });
     }
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args)
+    {
         AnimatedPictureViewer apv = new AnimatedPictureViewer();
     }
 
-    class Animate extends Thread{
-
-        public void run(){//begin run method
-            try{
-                //The following code will continue to
-                // loop until the animation thread is
-                // interrupted by the mouseExited 
-                // method.               
-                while(true){
-                    //Display several images in succession. 
-                    display(20);
-                }//end while loop  
-            }catch(Exception ex){
-                if(ex instanceof InterruptedException){
-                    //Do nothing. This exception is
-                    // expected on mouseExited.    
-                }else{//Unexpected exception occurred.
-                    System.out.println(ex);
-                    System.exit(1);//terminate program
-                }//end else                    
-            }//end catch              
-        }//end run          
-        //-----------------------------------------//
-
-        //This method displays an image and sleeps
-        // for a prescribed period of time.  It 
-        // terminates and throws an      
-        // InterruptedException when interrupted
-        // by the mouseExited method.     
-        void display(int delay)
-            throws InterruptedException{
-            //update the animation
-            component.repaint();
-            //Check interrupt status.  If interrupted
-            // while not asleep, force animation to
-            // terminate.              
-            if(Thread.currentThread().interrupted())
-                throw(new InterruptedException());
-            //Delay specified number of msec.
-            //Terminate animation automatically if
-            // interrupted while asleep.  
-            Thread.currentThread().sleep(delay);
-        }//end display method    
-        //-----------------------------------------//
-    }//end inner class named Animate     
+    class Animation implements Runnable
+    {
+        public boolean running = false; //Animates when true
+        
+        public void run()
+        {
+            running = true;
+            while(true)
+            {
+                if(running) //Draw next frame
+                    component.repaint();
+                try { //Delay between frames
+                    Thread.sleep(20);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
 }

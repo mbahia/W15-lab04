832232dd1e58140171037e61bc72c6cbe5ebe580
William Eiers <weiers@umail.ucsb.edu>
2015-02-20 10:25:12 -0800

    new branch

---
+++ src/edu/ucsb/cs56/w15/drawings/weiers/advanced/AnimatedPictureViewer.java
@@ -0,0 +1,83 @@
+package edu.ucsb.cs56.w15.drawings.weiers.advanced;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AnimatedPictureViewer {
+
+    private DrawPanel panel = new DrawPanel();
+    
+    Thread anim;   
+    
+    private int x = 100;
+    private int y = 100;
+    
+    private int dx = 5;
+    private int dy = -2;
+
+
+    public static void main (String[] args) {
+      new AnimatedPictureViewer().go();
+    }
+
+    public void go() {
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      frame.getContentPane().add(panel);
+      frame.setSize(640,480);
+      frame.setVisible(true);
+      
+      frame.getContentPane().addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e){
+                anim = new Animation();
+                anim.start();
+            }
+      });
+      
+    } // go()
+
+    class DrawPanel extends JPanel {
+       public void paintComponent(Graphics g) {
+
+        Graphics2D g2 = (Graphics2D) g;
+
+         // Clear the panel first
+          g2.setColor(Color.white);
+          g2.fillRect(0,0,this.getWidth(), this.getHeight());
+
+          // Draw the Ipod
+          g2.setColor(Color.RED);
+          PowerOutletWithGround outlet = new PowerOutletWithGround(x,y,60,90);
+          g2.draw(outlet);
+       }
+    }
+    
+    class Animation extends Thread {
+      public void run() {
+        try {
+          while (true) {
+            // Bounce off the walls
+
+            if (x >= 400) { dx = -5; }
+            if (x <= 50) { dx = 5; }
+            if (y <= 20) { dy = 3; }
+            if (y >= 300) { dy = -20; }
+            x += dx;                
+            y += dy;
+            panel.repaint();
+            Thread.sleep(50);
+          }
+        } catch(Exception ex) {
+          if (ex instanceof InterruptedException) {
+            // Do nothing - expected on mouseExited
+          } else {
+            ex.printStackTrace();
+            System.exit(1);
+          }
+        }
+      }
+    }
+    
+}

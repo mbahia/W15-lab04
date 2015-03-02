e71ea839f7867d5349b0c042bd348b9202607631
William Eiers <weiers@umail.ucsb.edu>
2015-03-01 23:01:00 -0800

    Last minute changes to animatedpictureviewer

--- src/edu/ucsb/cs56/w15/drawings/weiers/advanced/AnimatedPictureViewer.java
+++ src/edu/ucsb/cs56/w15/drawings/weiers/advanced/AnimatedPictureViewer.java
@@ -3,6 +3,19 @@ package edu.ucsb.cs56.w15.drawings.weiers.advanced;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
+
+/**
+    AnimatedPictureViewer
+    Based upon the outline of the AnimatedPictureViewer.java
+    made by andrewberls
+
+    This class draws two power outlets; one poweroutlet bounces
+    up and down, while the other bounces left and right
+
+    @author William Eiers
+    @version for CS56, W15, UCSB, 03/01/2015
+*/
 
 public class AnimatedPictureViewer {
 
@@ -15,7 +28,9 @@ public class AnimatedPictureViewer {
     
     private int dx = 5;
     private int dy = -2;
-
+    
+    // for random color
+    private Random rand = new Random();
 
     public static void main (String[] args) {
       new AnimatedPictureViewer().go();
@@ -28,7 +43,8 @@ public class AnimatedPictureViewer {
       frame.getContentPane().add(panel);
       frame.setSize(640,480);
       frame.setVisible(true);
-      
+      // clicking the mouse button starts te animation; wont stop until
+      // the application is closed
       frame.getContentPane().addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e){
                 anim = new Animation();
@@ -47,10 +63,13 @@ public class AnimatedPictureViewer {
           g2.setColor(Color.white);
           g2.fillRect(0,0,this.getWidth(), this.getHeight());
 
-          // Draw the Ipod
-          g2.setColor(Color.RED);
-          PowerOutletWithGround outlet = new PowerOutletWithGround(x,y,60,90);
+          // Draw two power outlet, one going up and down, other goind left/right
+          // with a randomized color each time
+          g2.setColor(new Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255)));
+          PowerOutletWithGround outlet = new PowerOutletWithGround(300,y,60,90);
+          PowerOutletWithGround outlet2 = new PowerOutletWithGround(x, 200, 60,90);
           g2.draw(outlet);
+          g2.draw(outlet2);
        }
     }
     
@@ -59,11 +78,12 @@ public class AnimatedPictureViewer {
         try {
           while (true) {
             // Bounce off the walls
-
-            if (x >= 400) { dx = -5; }
-            if (x <= 50) { dx = 5; }
+            // both up and down, including corners
+            if (x >= 600) { dx = -5; }
+            if (x <= 20) { dx = 3; }
             if (y <= 20) { dy = 3; }
-            if (y >= 300) { dy = -20; }
+            if (y >= 300) { dy = -3; }
+            // change the velocities of the outlets
             x += dx;                
             y += dy;
             panel.repaint();
